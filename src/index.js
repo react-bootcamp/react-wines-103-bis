2939cf90cb432e6b338363a5f2c31b3b5a8fb81a
Mathieu <mathieu.ancelin@gmail.com>
2017-06-01 09:13:21 +0200

    Fix routing issues on giithub

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 import { reducers } from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import createHistory from 'history/createBrowserHistory'
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { WineApp } from './components';
 import './index.css';
 
@@ -20,20 +20,16 @@ if (!window.Symbol) {
 const root = window.location.hostname === 'react-bootcamp.github.io'
   ? '/react-wines-103-bis/'
   : '/';
-const history = createHistory()
-const middleware = routerMiddleware(history)
+
 const store = createStore(
-  combineReducers({
-    ...reducers,
-    router: routerReducer
-  }),
-  applyMiddleware(middleware, thunk)
+  combineReducers({ ...reducers }),
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render((
   <Provider store={store}>
-    <ConnectedRouter basename={root} history={history}>
+    <Router basename={root}>
       <WineApp />
-    </ConnectedRouter>
+    </Router>
   </Provider>
 ), document.getElementById('root'));
